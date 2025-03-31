649c7e40a8a805ee26e42bdf6139177b8b74226c
sjlvanq <sjlvanq@gmail.com>
2025-03-31 10:39:05 -0300

    enclose in method main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from sys import exit
-from numpy import full
-
 import pygame
+from numpy import full
+from sys import exit
+
 import tileset, tilespalette, canvas
 
 SCREEN_WIDTH = 500
@@ -12,23 +12,28 @@ TILE_WIDTH = 32
 TILE_HEIGHT = 32
 MAP = full((10,10), -1)
 
-pygame.init()
-screen = pygame.display.set_mode((500, 350))
-print("Tileset image used in this prototype by gfx0 at OpenGameArt.Org\nhttps://opengameart.org/users/gfx0")
+def main():
 
-tset = tileset.Tileset(TILESET, TILE_WIDTH, TILE_HEIGHT)
-tpal = tilespalette.TilesPalette(PALETTE_COLUMNS, SCREEN_HEIGHT, tset)
-canv = canvas.Canvas(tpal.surface.get_rect().topright, MAP, tset)
+	pygame.init()
+	screen = pygame.display.set_mode((500, 350))
+	print("Tileset image used in this prototype by gfx0 at OpenGameArt.Org\nhttps://opengameart.org/users/gfx0")
 
-while True:
-	events = pygame.event.get()
-	for e in events:
-		if e.type == pygame.QUIT:
-			exit()
+	tset = tileset.Tileset(TILESET, TILE_WIDTH, TILE_HEIGHT)
+	tpal = tilespalette.TilesPalette(PALETTE_COLUMNS, SCREEN_HEIGHT, tset)
+	canv = canvas.Canvas(tpal.surface.get_rect().topright, MAP, tset)
 
-	tpal.update(events)
-	canv.update(events, tpal.canvas.selected_tile)
+	while True:
+		events = pygame.event.get()
+		for e in events:
+			if e.type == pygame.QUIT:
+				exit()
 
-	screen.blit(tpal.surface, ((0,0)))
-	screen.blit(canv.surface, canv.rect)
-	pygame.display.update()
+		tpal.update(events)
+		canv.update(events, tpal.canvas.selected_tile)
+
+		screen.blit(tpal.surface, ((0,0)))
+		screen.blit(canv.surface, canv.rect)
+		pygame.display.update()
+
+if __name__ == '__main__':
+	main()
